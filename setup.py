43de1ea50739f868e0eecc4b90dd6742330bf558
ID1OS <154928580+ID1OS@users.noreply.github.com>
2023-12-27 07:22:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ import subprocess
 
 env = {}
 User =subprocess.run("whoami", capture_output=True).stdout.decode("utf-8").strip()
-home_directory = f"/home/{User}" if User != "root" else "/root"
+if User == "root":
+    home_directory = "/root"
+else:
+    home_directory = f"/home/{User}"
 env["root"] = f"{home_directory}/.id1fs/ID1FS"
 env["bf"] = f"{home_directory}/.id1fs/ID1FS/backup"
 env["md"] = f"{home_directory}/.id1fs/ID1FS/metadata"
